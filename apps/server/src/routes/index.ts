91b8aee3a2e888714c27b438d6af298fa1c7c3c9
PrathamSingh1 <prathamjii36@gmail.com>
2026-07-14 16:48:19 +0000

    added routing

--- apps/server/src/routes/index.ts
+++ apps/server/src/routes/index.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import authRoutes from "../features/auth/auth.routes"
+import questionRoutes from "../features/questions/questions.routes"
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
 
-// Future routes:
-// router.use("/questions", questionRoutes);
+router.use("/questions", questionRoutes);
 // router.use("/premium", premiumRoutes);
 // router.use("/referral", referralRoutes);
 // router.use("/experiences", experienceRoutes);
