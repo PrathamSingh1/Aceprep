62de157883f6c26963187063e3eb0b52f3aa6089
PrathamSingh1 <prathamjii36@gmail.com>
2026-07-14 14:43:40 +0000

    added backend route

--- apps/server/src/routes/index.ts
+++ apps/server/src/routes/index.ts
@@ -1,9 +1,17 @@
 import express from "express";
-
+import authRoutes from "../features/auth/auth.routes"
 
 const router = express.Router();
 
-router.use("/auth");
-router.use("/questions");
+router.use("/auth", authRoutes);
+
+// Future routes:
+// router.use("/questions", questionRoutes);
+// router.use("/premium", premiumRoutes);
+// router.use("/referral", referralRoutes);
+// router.use("/experiences", experienceRoutes);
+// router.use("/admin", adminRoutes);
+
+
 
 export default router;
