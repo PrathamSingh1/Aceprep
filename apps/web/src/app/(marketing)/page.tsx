00edd5a94ec5a54f704418cc8fac511bfee47d96
PrathamSingh1 <prathamjii36@gmail.com>
2026-07-28 04:48:06 +0530

    added heading component

--- apps/web/src/app/(marketing)/page.tsx
+++ apps/web/src/app/(marketing)/page.tsx
@@ -1,8 +1,9 @@
+import { Hero } from "@/components/layout/hero";
 
 export default function Home() {
   return (
     <div className="min-h-screen font-display">
-
+      <Hero />
     </div>
   );
 }
---
+++ apps/web/src/components/layout/hero.tsx
@@ -0,0 +1,13 @@
+import { Heading } from "../ui/heading"
+import { Container } from "./container"
+
+
+export const Hero = () => {
+  return (
+    <div>
+      <Container>
+        <Heading>Interview Prepration as Next Level</Heading>
+      </Container>
+    </div>
+  )
+}
