54a206f22b391e16f1d576bab0880c71b5e0e526
PrathamSingh1 <prathamjii36@gmail.com>
2026-07-28 04:35:38 +0530

    added navbar

--- apps/web/src/components/layout/logo.tsx
+++ apps/web/src/components/layout/logo.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link"
 
 
 export const Logo = () => {
   return (
     <div className="text-lg">
-      Aceprep
+      <Link href="/">
+        <h3>Aceprep</h3>
+      </Link>
     </div>
   );
 }
